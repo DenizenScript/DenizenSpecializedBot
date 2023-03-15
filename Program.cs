3f6b27bd2803b3cd3dd56b125d55a6f6085a44bb
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-03-15 11:09:43 -0700

    fix roles check

--- Program.cs
+++ Program.cs
@@ -711,7 +711,7 @@ public static  class Program
                         return;
                     }
                     Console.WriteLine($"Chatter message from {message.Author.Id} (last notified = {LastUserNotifiedChannelRedir}), (roles = {(message.Author is SocketGuildUser udebug ? string.Join(",", udebug.Roles.Select(s => s.Id)) : "")} of {message.Content}");
-                    if (message.Author is not SocketGuildUser user || user.Roles.Any())
+                    if (message.Author is not SocketGuildUser user || user.Roles.Any(r => r.Id == 521680043685052418ul)) // NotSilent role
                     {
                         return;
                     }
